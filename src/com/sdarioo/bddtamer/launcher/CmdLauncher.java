2a7f7d9d8ea5410b1ca562f84f8456e8fba666c6
sdarioo <sdarioo@github.com>
2016-10-26 22:26:01 +0200

    Working on cmd launcher.

--- src/com/sdarioo/bddtamer/launcher/CmdLauncher.java
+++ src/com/sdarioo/bddtamer/launcher/CmdLauncher.java
@@ -5,11 +5,14 @@ import com.sdarioo.bddtamer.model.Scenario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class CmdLauncher extends AbstractLauncher {
 
@@ -21,8 +24,10 @@ public class CmdLauncher extends AbstractLauncher {
         Path path = null;
         try {
             path = writeToTempFile(scenario);
+            StringBuilder sb = new StringBuilder();
+            execute(new String[] { "java", "-version"}, s->sb.append(s+'\n'), s->sb.append(s+'\n'));
 
-            return new TestResult(RunStatus.Passed, 100L, path.toString());
+            return new TestResult(RunStatus.Passed, 100L, path.toString() + '\n' + sb.toString());
         } catch (IOException e) {
             return new TestResult(RunStatus.Failed, 0L, e.toString());
         } finally {
@@ -47,6 +52,29 @@ public class CmdLauncher extends AbstractLauncher {
             } catch (IOException e) {
                 LOGGER.warn(e.toString());
             }
+        }
+    }
+
+    private static int execute(String[] command, Consumer<String> out, Consumer<String> err) {
+        try {
+            Process proc = Runtime.getRuntime().exec(command);
+
+            String line;
+            try (BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()))) {
+                while ((line = stdInput.readLine()) != null) {
+                    out.accept(line);
+                }
+            }
+            try (BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()))) {
+                while ((line = stdError.readLine()) != null) {
+                    err.accept(line);
+                }
+            }
+            return proc.waitFor();
+
+        } catch (IOException | InterruptedException e) {
+            err.accept(e.toString());
+            return -1;
         }
     }
 
